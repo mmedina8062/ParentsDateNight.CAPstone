5da900c61aca5ad4c028718b41f3e189b4d858ad
unknown <mmedina8062@gmail.com>
2019-10-22 15:33:30 -0500

    added Parents Model to create a database that holds all info needed such as full name and full address

---
+++ ParentsDateNight/Models/Parent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ParentsDateNight.Models
+{
+    public class Parent
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Street")]
+        public int StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+    }
+}
